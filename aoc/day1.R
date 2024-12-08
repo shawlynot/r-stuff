7c7acf605e9397900346dfb84966081700009e35
shawlynot <88781957+shawlynot@users.noreply.github.com>
2024-12-08 16:32:45 +0000

    day1 part 2

--- aoc/day1.R
+++ aoc/day1.R
@@ -1,5 +1,20 @@
 input <- read.table("input/day1.txt", col.names = (c("left", "right")))
+
+# part 1
 left <- sort(input$left)
 right <- sort(input$right)
 distance <- abs(left - right)
 print(sum(distance))
+
+# part 2
+left_uniq <- unique(left)
+right_freq <- table(right)
+
+ss <- 0
+for (l in left_uniq) {
+    r <- right_freq[toString(l)]
+    if (!is.na(r)){
+        ss <- ss + (l * r)
+    }
+}
+print(ss)
